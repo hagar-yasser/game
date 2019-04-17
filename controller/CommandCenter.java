1342419e8a033594ef4d2dcfa1b224d536c06782
hagar-yasser <47675424+hagar-yasser@users.noreply.github.com>
2019-04-17 17:13:25 +0200

    Update CommandCenter.java

--- controller/CommandCenter.java
+++ controller/CommandCenter.java
@@ -2,33 +2,57 @@ package controller;
 
 import java.util.ArrayList;
 
-import model.SOSListener;
+import model.events.SOSListener;
 import model.infrastructure.ResidentialBuilding;
 import model.people.Citizen;
 import model.units.Unit;
 import simulation.Rescuable;
 import simulation.Simulator;
+import views.MainLook;
 
 public class CommandCenter implements SOSListener {
 
 	private Simulator engine;
 	private ArrayList<ResidentialBuilding> visibleBuildings;
 	private ArrayList<Citizen> visibleCitizens;
+
+	@SuppressWarnings("unused")
 	private ArrayList<Unit> emergencyUnits;
+	private MainLook mainLook;
 
 	public CommandCenter() throws Exception {
-
-		engine = new Simulator();
-        visibleBuildings = new ArrayList<ResidentialBuilding>();
+	    mainLook=new MainLook();
+		engine = new Simulator(this);
+		engine.setMainLookListener(mainLook);
+		visibleBuildings = new ArrayList<ResidentialBuilding>();
 		visibleCitizens = new ArrayList<Citizen>();
-		emergencyUnits = new ArrayList<Unit>();
+		emergencyUnits = engine.getEmergencyUnits();
 
 	}
-	 public void receiveSOSCall(Rescuable r){
-		if(r instanceof Citizen)
-			visibleCitizens.add((Citizen)r);
-         if(r instanceof ResidentialBuilding)
-             visibleBuildings.add((ResidentialBuilding)r);
 
-     }
+	@Override
+	public void receiveSOSCall(Rescuable r) {
+		
+		if (r instanceof ResidentialBuilding) {
+			
+			if (!visibleBuildings.contains(r)) {
+                visibleBuildings.add((ResidentialBuilding) r);
+
+                mainLook.getPanelsinGrid()[r.getLocation().getX()][r.getLocation().getY()].getRescButton().addTarget(r);
+                mainLook.getPanelsinGrid()[r.getLocation().getX()][r.getLocation().getY()].getRescButton().addDisaster(r.getDisaster());
+            }
+
+			
+		} else {
+			
+			if (!visibleCitizens.contains(r)) {
+                visibleCitizens.add((Citizen) r);
+                mainLook.getPanelsinGrid()[r.getLocation().getX()][r.getLocation().getY()].getRescButton().addTarget(r);
+                mainLook.getPanelsinGrid()[r.getLocation().getX()][r.getLocation().getY()].getRescButton().addDisaster(r.getDisaster());
+            }
+
+		}
+
+	}
+
 }
